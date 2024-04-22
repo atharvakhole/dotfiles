e55da91e58af16260b843f3ca3570a802b036fd7
Atharva Khole <atharvakhole26@gmail.com>
2024-04-22 09:16:07 +0800

    Add Neovim config files

---
+++ nvim/after/plugin/packer.lua
@@ -0,0 +1,118 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    -- Nvim-tree
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    }
+
+    use('theprimeagen/harpoon')
+
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use { "ellisonleao/gruvbox.nvim" }
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+    -- debugger (DAP)
+    use { "mfussenegger/nvim-dap" }
+    use { "rcarriga/nvim-dap-ui" }
+
+    -- rust-tools.nvim
+    use { "simrat39/rust-tools.nvim" }
+
+    -- auto-pairs
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+
+    -- auto-ts-tag for html/tsx tag completion
+    use "windwp/nvim-ts-autotag"
+    require("nvim-ts-autotag").setup()
+
+    -- lualine
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
+    -- Comment.nvim
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+
+    -- indent-blankline
+    use { "lukas-reineke/indent-blankline.nvim" }
+
+    -- lsp
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
+    }
+    -- Completion framework:
+    use 'hrsh7th/nvim-cmp'
+
+    -- LSP completion source:
+    use 'hrsh7th/cmp-nvim-lsp'
+
+    -- Useful completion sources:
+    use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-nvim-lsp-signature-help'
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/vim-vsnip'
+    use 'uga-rosa/cmp-dictionary'
+
+    -- Wrapping plaintext files
+    use "andrewferrier/wrapping.nvim"
+    require("wrapping").setup()
+
+    -- Markdown preview
+    -- install without yarn or npm
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
+    --
+    -- use({
+    --     "iamcco/markdown-preview.nvim",
+    --     run = "cd app && npm install",
+    --     setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    --     ft = { "markdown" },
+    -- })
+end)
---
+++ nvim/lua/atharva/remap.lua
@@ -0,0 +1,22 @@
+vim.g.mapleader = " "
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("i", "kj", "<Esc>")
+
+-- control backspace deletes word in insert mode
+vim.keymap.set("i", "<C-H>", "<C-w>")
+
+-- Visual block move
+
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+
+-- Move by display lines in wrapped text
+keymap("n", "j", "gj", opts)
+keymap("n", "gj", "j", opts)
+keymap("n", "k", "gk", opts)
+keymap("n", "gk", "k", opts)
